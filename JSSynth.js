a2c0d9b84b88d3c214c4dee4fb01c213f3ece25f
Ray Redondo <rdrpenguin04@users.noreply.github.com>
2018-08-28 08:03:58 -0500

    Create JSSynth.js

---
+++ JSSynth.js
@@ -0,0 +1,15 @@
+var context;
+
+function initJSSynth() {
+  window.addEventListener('load', init, false);
+  function init() {
+    try {
+      // Fix up for prefixing
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      context = new AudioContext();
+    }
+    catch(e) {
+      alert('Web Audio API is not supported in this browser');
+    }
+  }
+}
