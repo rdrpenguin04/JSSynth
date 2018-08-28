7eedc0fcd0f79e183537ae58c32024cee936afb7
Ray Redondo <rdrpenguin04@users.noreply.github.com>
2018-08-28 11:07:07 -0500

    Update JSSynth.js

--- JSSynth.js
+++ JSSynth.js
@@ -1,18 +1,16 @@
 var JSSynthContext;
 var JSSynthReady = false;
 
-function initJSSynth() {
-  console.log("Starting JSSynth version 1.0 by Ray Redondo...");
-  function init() {
-    try {
-      // Fix up for prefixing
-      window.AudioContext = window.AudioContext||window.webkitAudioContext;
-      context = new AudioContext();
-    }
-    catch(e) {
-      alert('Web Audio API is not supported in this browser');
-    }
-    JSSynthReady = true;
+console.log("Starting JSSynth version 1.0 by Ray Redondo...");
+function init() {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
   }
-  window.addEventListener('load', init, false);
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+  JSSynthReady = true;
 }
+window.addEventListener('load', init, false);
